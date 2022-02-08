80753104ad4c0a62eb112fc3b9ea6284213f6d0b
Lunara <cc21106@g.unicamp.br>
2022-02-08 02:53:51 -0300

    atualziacao1
    consertando o meu_express pra rodar no terminal do vscode

--- src/config/meu_express.js
+++ src/config/meu_express.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const path = require("path")
-const methodOverride = require("method-override");
 const bodyParser = require("body-parser");
 const morgan = require('morgan');
 const {sequelize} = require('./database');
@@ -20,14 +19,6 @@ const rotas = require("../app/rotas/rotas");
 rotas(app);
 
 app.use("/estatico", express.static("src/app/views/usuarios"));
-
-app.use(methodOverride(function (req,res) {
-  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-  var method = req.body._method;
-  delete req.body._method;
-  return method;
-  }
-}));
 
 app.get("/add-usuario", function(req,res){
 res.send("Formulário para cadastrar usuários")
