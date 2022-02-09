a7d36dd1d44d30c71b3e4a857bdc49533ad1e480
Lunara <cc21106@g.unicamp.br>
2022-02-09 18:56:39 -0300

    atualizacao parte11
    atualizacao parte11

--- src/app/controladores/historico_controllers.js
+++ src/app/controladores/historico_controllers.js
@@ -7,7 +7,7 @@ class HistoricoControllers
         return function (req, res) {
             const eventoDB = new Eventos_bd(db);
             eventoDB.selecionaEventoDoCarrinho(function (err, resultadosEvento) {
-                eventoDB.selecionaEvento(function (err, resultadosHistorico, {eventosSelecionados:{}}) {
+                eventoDB.selecionaEvento(function (err, resultadosHistorico) {
                     var historico_c = resultadosHistorico[0].historico;
                     var valorTotalP = 0;
                     var valorTotalF = 0;
@@ -16,8 +16,8 @@ class HistoricoControllers
                         valorTotalF = valorTotalF + resultadosEvento[i].totalDoEvento;
                     }
                     console.log('Acessou historico logout...')
-                    res.render('historico.ejs',{
-                        produtosSelecionados: resultadosEvento,
+                    res.render('historico.ejs', {eventosSelecionados:{}},{
+                        eventosSelecionados: resultadosEvento,
                         valorTotalF: valorTotalF,
                         temHistorico: historico_c
                     })
@@ -31,8 +31,8 @@ class HistoricoControllers
             eventoDB.deletaHistorico(function (err, resultadosEvento) {
             eventoDB. selecionaHistoricoCarrinho(function (err, resultadosHistorico) {
                         var historico_c = resultadosHistorico[0].historico;
-                        res.render('Historico.ejs',{
-                            produtosSelecionados: resultadosEvento,
+                        res.render('Historico.ejs', {eventosSelecionados:{}},{
+                            eventosSelecionados: resultadosEvento,
                             valorTotalF: 0,
                             temHistorico: historico_c 
                         })
