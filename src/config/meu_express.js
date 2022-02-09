6ffed10bee18c6f1c10f79be739e343f3d433f18
Lunara <cc21106@g.unicamp.br>
2022-02-09 15:48:39 -0300

    colocando funcao para captura de erros no meu_express
    colocando funcao para captura de erros no meu_express

--- src/config/meu_express.js
+++ src/config/meu_express.js
@@ -21,5 +21,23 @@ app.use('/caminhoImagem5',express.static('src/app/views/imagens'));
 app.set("views", path.join(__dirname,'../app/views'));
 app.set("view engine", "ejs");
 
+// Para capturar os erros se houverem durante as rotas:
+app.use((req, res, next) => {
+    const render = res.render;
+    const send = res.send;
+    res.render = function renderWrapper(...args) {
+        Error.captureStackTrace(this);
+        return render.apply(this, args);
+    };
+    res.send = function sendWrapper(...args) {
+        try {
+            send.apply(this, args);
+        } catch (err) {
+            console.error(`Error in res.send | ${err.code} | ${err.message} | ${res.stack}`);
+        }
+    };
+    next();
+});
+
 
 module.exports = app;
